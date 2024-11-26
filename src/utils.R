bf13c96d7546c27401ba189f38c242ad1e692597
thomas-tams-dtu <s204540@dtu.dk>
2024-11-26 09:11:31 +0100

    build: more utils

--- src/utils.R
+++ src/utils.R
@@ -1,18 +1,18 @@
 ## For retrieving aa sequences from ENSTs
 get_aa_seqs_from_ENSTs <- function(transcript_ids, batch_size = 100, seqType = "peptide") {
   # Get human ensembl mart
-  cat("Creating human mart from biomaRt::useMart with hsapiens_gene_ensembl \n")
+  message("Creating human mart from biomaRt::useMart with hsapiens_gene_ensembl \n")
   ensembl_hsap <- biomaRt::useMart("ensembl", dataset = "hsapiens_gene_ensembl")
   
   # Initialize an empty tibble to store results
-  results <- tibble()
+  results <- tibble::tibble()
   
   # Split transcript IDs into batches
   id_batches <- split(transcript_ids, ceiling(seq_along(transcript_ids) / batch_size))
   
   # Loop over each batch
   for (i in seq_along(id_batches)) {
-    cat("Processing batch", i, "of", length(id_batches), "\n")
+    message("Processing batch", i, "of", length(id_batches), "\n")
     
     # Query the current batch
     batch_result <- biomaRt::getSequence(
@@ -28,10 +28,19 @@ get_aa_seqs_from_ENSTs <- function(transcript_ids, batch_size = 100, seqType = "
   return(results)
 }
 
-seqs |> head(2) |> pull(peptide)
+## For writing fastas
+write_fasta_from_named_list <- function(sequences, file_name) {
+  # Create a named DNAStringSet or AAStringSet object
+  seq_set <- Biostrings::AAStringSet(sequences)
+  
+  # Write the DNAStringSet object to a FASTA file
+  Biostrings::writeXStringSet(seq_set, filepath = file_name)
+  
+  message("FASTA file written to: ", file_name)
+}
 
-# For reading gff3 output from deepTMHMM
-read_gff3 <- function(file_path) {
+### For reading gff3 output from deepTMHMM
+tidy_read_gff3 <- function(file_path) {
   # Read the file
   lines <- readLines(file_path)
   
@@ -88,11 +97,3 @@ read_gff3 <- function(file_path) {
   
   return (parsed_data)
 }
-
-
-all_projects  <-recount3::available_projects()
-all_projects |>
-  as_tibble() |>
-  filter(project_home == "data_sources/tcga") |>
-  filter()
-  distinct(project_home)
