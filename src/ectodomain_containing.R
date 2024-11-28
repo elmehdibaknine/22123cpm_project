0463c9da54b4f99812669c5d832c32206fdb4e15
thomas-tams-dtu <s204540@dtu.dk>
2024-11-28 12:19:43 +0100

    wip

--- src/ectodomain_containing.R
+++ src/ectodomain_containing.R
@@ -3,4 +3,20 @@ source(here("src/utils.R"))
 library(tidyverse)
 
 # Read in the gff3 results from deeptmhmm
-tidy_read_gff3(here("data/processed/test.gff3"))
+deeptmhmm_tidy <- tidy_read_gff3(here("data/processed/TMRs.gff3")) |>
+  janitor::clean_names()
+
+# Filter transcripts with ectodomain larger than threshold
+ectodomain_length_threshold <- 50
+transcript_above_threshold <- deeptmhmm_tidy |>
+  group_by(protein_id) |>
+  filter(feature_type == "outside") |>
+  mutate(ectodomain_length = end - start) |>
+  filter(ectodomain_length > ectodomain_length_threshold) |>
+  ungroup() |>
+  distinct(protein_id) |>
+  pull(protein_id)
+
+  
+deeptmhmm_tidy |>
+  filter(protein_id %in% transcript_above_threshold)
