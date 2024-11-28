929de550c4dde7c9e96ef725bc0890bfb450db98
thomas-tams-dtu <s204540@dtu.dk>
2024-11-28 15:31:11 +0100

    fix: rename protein_id to transcript in datafiles

--- src/03_ectodomain_containing.R
+++ src/03_ectodomain_containing.R
@@ -4,31 +4,32 @@ library(tidyverse)
 
 # Read in the gff3 results from deeptmhmm
 deeptmhmm_tidy <- tidy_read_gff3(here("data/processed/TMRs.gff3")) |>
-  janitor::clean_names()
+  janitor::clean_names() |>
+  dplyr::rename(transcript = protein_id)
 
 # Filter transcripts with ectodomain larger than threshold
 ectodomain_length_threshold <- 50
 deeptmhmm_tidy_above_threshold <- deeptmhmm_tidy |>
-  group_by(protein_id) |>
+  group_by(transcript) |>
   filter(feature_type == "outside") |>
   mutate(ectodomain_length = end - start) |>
   filter(ectodomain_length > ectodomain_length_threshold) |>
   ungroup() |>
-  distinct(protein_id)
+  distinct(transcript)
 
 deeptmhmm_tidy_above_threshold |>
   write_tsv(here("data/processed/ectodomain_containing_transcript.tsv"))
 
 # Pull a list of protein ids above threshold
 transcript_above_threshold <- deeptmhmm_tidy_above_threshold |>
-  pull(protein_id)
+  pull(transcript)
 
 # Plot selected transcripts predicted topology features
 deeptmhmm_tidy |>
-  filter(protein_id %in% transcript_above_threshold[1:9]) |>
+  filter(transcript %in% transcript_above_threshold[1:9]) |>
   ggplot(aes(xmin = start, xmax = end, ymin = 0, ymax = 1, fill = feature_type)) +
   geom_rect() +
-  facet_wrap(~ protein_id, scales = "free_y") +   # Facet by transcript (protein_id)
+  facet_wrap(~ transcript, scales = "free_y") +   # Facet by transcript (transcript)
   scale_fill_manual(values = c("signal" = "orange", "outside" = "green", "TMhelix" = "darkgrey", "inside" = "red")) + # Customize colors
   theme_minimal() +
   theme(
