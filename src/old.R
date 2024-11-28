0a38aa29a0e87480a1d270bcf813f6bc39b0bcc1
thomas-tams-dtu <s204540@dtu.dk>
2024-11-28 14:50:35 +0100

    mv: old code

--- src/old.R
+++ src/old.R
@@ -43,3 +43,34 @@ harmonizeGtex <- function(gtex_tpm_path, gtex_count_path, tpm_harmon_out, count_
   
   return()
 }
+
+
+
+
+
+
+########
+# deeploc_subset.R
+#####
+
+### Subsetting using deeploc results
+cell_membrane_proteins <- read_tsv(file = ("data/raw/cell_membrane_proteins_enst.txt"))
+
+tcga_counts <- read_tsv(file = ("data/raw/filtered_TCGA.gz"))
+enst_list <- cell_membrane_proteins |> select(enst)
+
+tcga_counts_m <- tcga_counts |> 
+  mutate(sample_simple = str_remove(sample, "\\.\\d+")) 
+
+
+tcga_counts_subset <- tcga_counts_m |>
+  inner_join(, y = enst_list, by = c("sample_simple" = "enst"))
+
+### Removing rows with column sum below 10
+tcga_counts_subset_2 <- tcga_counts_subset |> 
+  rowwise() |>
+  filter(sum(c_across(where(is.numeric))) >= 10) |>
+  ungroup()
+
+### Writing file to disk
+tcga_counts_subset_2 |> write_tsv(file = "data/processed/tcga_counts_subset.tsv.gz")
